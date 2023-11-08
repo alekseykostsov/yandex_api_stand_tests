24328550390ce9213b6b2e89b07867a41bb3b5d1
Aleksey Kostsov <akoscov89@yandex.ru>
2023-11-08 19:09:53 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://be07ec73-fabe-4e64-8b0d-d662f928200b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
